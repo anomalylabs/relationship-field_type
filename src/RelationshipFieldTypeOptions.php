0c73db44d6f1105c6579be7a4365dced976cb8c9
Ryan Thompson <service@aiwebsystems.com>
2015-04-15 13:00:16 -0500

    Placeholder

--- src/RelationshipFieldTypeOptions.php
+++ src/RelationshipFieldTypeOptions.php
@@ -29,7 +29,7 @@ class RelationshipFieldTypeOptions
 
         $query = $model->newQuery();
 
-        return [null => trans($fieldType->getPlaceholder()) . '...'] +
+        return [null => trans($fieldType->getPlaceholder())] +
         $query->get()->lists(
             array_get($fieldType->getConfig(), 'title', $model->getTitleName()),
             array_get($fieldType->getConfig(), 'key', $model->getKeyName())
