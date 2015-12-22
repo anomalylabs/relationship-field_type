d53a31994e3ce2da262d022f279179fc4d877adc
Ryan Thompson <service@aiwebsystems.com>
2015-12-21 18:44:49 -0600

    Fix presenter logic

--- src/RelationshipFieldTypePresenter.php
+++ src/RelationshipFieldTypePresenter.php
@@ -13,48 +13,4 @@ use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
 class RelationshipFieldTypePresenter extends FieldTypePresenter
 {
 
-    /**
-     * Fallback to getting attributes
-     * off the related value.
-     *
-     * @param string $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        if ($return = parent::__get($key)) {
-            return $return;
-        }
-
-        if ($related = $this->object->getValue()) {
-            if ($return = self::__getDecorator()->decorate($related)->{$key}) {
-                return $return;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Fallback to calling methods
-     * on the related value.
-     *
-     * @param string $method
-     * @param array  $arguments
-     * @return mixed
-     */
-    public function __call($method, $arguments)
-    {
-        if ($return = parent::__call($method, $arguments)) {
-            return $return;
-        }
-
-        if ($related = $this->object->getValue()) {
-            if ($return = call_user_func_array([self::__getDecorator()->decorate($related), $method], $arguments)) {
-                return $return;
-            }
-        }
-
-        return null;
-    }
 }
