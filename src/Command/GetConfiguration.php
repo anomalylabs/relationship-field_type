c07fa8288f76672b74655b4f241319a4ae8fe880
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 00:39:50 -0600

    Author

--- src/Command/GetConfiguration.php
+++ src/Command/GetConfiguration.php
@@ -7,9 +7,9 @@ use Illuminate\Contracts\Cache\Repository;
 /**
  * Class GetConfiguration
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
  * @package       Anomaly\RelationshipFieldType\Command
  */
 class GetConfiguration implements SelfHandling
