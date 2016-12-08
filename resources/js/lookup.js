ab88cec8fee3e448f85ff2f661bbdb5444ac2ba3
samharrison7 <samharrison.xg@gmail.com>
2016-12-08 11:37:04 +0000

    Making work on subfolder installations

--- resources/js/lookup.js
+++ resources/js/lookup.js
@@ -12,7 +12,7 @@ $(function () {
 
             e.preventDefault();
 
-            wrapper.find('.selected').load('/streams/relationship-field_type/selected/' + $(this).data('key') + '?uploaded=' + $(this).data('entry'), function () {
+            wrapper.find('.selected').load(REQUEST_ROOT_PATH + '/streams/relationship-field_type/selected/' + $(this).data('key') + '?uploaded=' + $(this).data('entry'), function () {
                 modal.modal('hide');
             });
 
