45a122bca0a995aeebf12025212b6eb6fe9bfc05
Ryan Thompson <service@aiwebsystems.com>
2017-09-06 18:06:08 -0500

    Handler for config

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,31 +1,14 @@
 <?php
 
-use Anomaly\Streams\Platform\Stream\Contract\StreamInterface;
+use Anomaly\RelationshipFieldType\Support\Config\RelatedHandler;
 
 return [
     'related' => [
         'required' => true,
         'type'     => 'anomaly.field_type.select',
         'config'   => [
-            'options' => function (\Anomaly\Streams\Platform\Stream\Contract\StreamRepositoryInterface $streams) {
-
-                $options = [];
-
-                /* @var StreamInterface as $stream */
-                foreach ($streams->visible() as $stream) {
-                    $options[ucwords(str_replace('_', ' ', $stream->getNamespace()))][$stream->getEntryModelName(
-                    )] = $stream->getName();
-                }
-
-                foreach ($options as $namespace) {
-                    ksort($namespace);
-                }
-
-                ksort($options);
-
-                return $options;
-            }
-        ]
+            'handler' => RelatedHandler::class,
+        ],
     ],
     'mode'    => [
         'required' => true,
@@ -35,7 +18,7 @@ return [
                 'dropdown' => 'anomaly.field_type.relationship::config.mode.option.dropdown',
                 'lookup'   => 'anomaly.field_type.relationship::config.mode.option.lookup',
                 'search'   => 'anomaly.field_type.relationship::config.mode.option.search',
-            ]
-        ]
-    ]
+            ],
+        ],
+    ],
 ];
